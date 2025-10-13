607c7156a84d0783949b246c864eca81f5f48781
синечка ♡ <63897347+ktnk-dev@users.noreply.github.com>
2025-10-14 00:11:30 +0300

    test

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,3 +1,11 @@
+from models.bot import bot
+from telebot.handler_backends import ContinueHandling
+
+@bot.message_handler(func=lambda _: True)
+def middleware(message): 
+    print(message.from_user.id, message.text)
+    return ContinueHandling()
+
 from . import (
     start,
     default,
@@ -5,7 +13,7 @@ from . import (
     remove_friend,
     # send_all_bd
 )
-from models.bot import bot
+
 
 def start_bot():
     print("Бот запущен...")
